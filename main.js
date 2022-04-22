ba051f3fdab2a43b86a2dbc0f3d2a53bd0195dcb
latte-y <yukimkyn1119@gmail.com>
2022-04-23 08:05:20 +0900

    ポートフォリオサイトの編集

---
+++ main.js
@@ -0,0 +1,14 @@
+const pics_src = ["photo1.jpg","photo2.jpg","photo3.jpg"];
+let num = -1;
+ 
+function slideshow_timer(){
+  if (num === 2){
+    num = 0;
+  } 
+  else {
+    num ++;
+  }
+  document.getElementById("mypic").src = pics_src[num];
+}
+ 
+setInterval(slideshow_timer, 2000);
